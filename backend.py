13aa0baf8d0cc4a0e1a2ffd200eb440794fdd987
m-spunky <mayurdjadhav1@gmail.com>
2024-02-20 11:14:21 +0530

    v3

--- backend.py
+++ backend.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
-GoogleGenerativeAIEmbeddings(model="models/embedding-001")  # Initialize the embeddings globally
+GoogleGenerativeAIEmbeddings(model="models/embedding-001")  
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
@@ -110,6 +110,76 @@ def get_question():
         "answer": ans
     }
     return jsonify(response)
+
+
+def prompt_for_story():
+    prompt = """
+    Generate a story prompt based on the provided parameters.
+    Parameters:
+    - num_characters (int): The number of characters in the story.
+    - synopsis (str): A brief overview of the story plot.
+    - environments (list): A list of key environments or settings in the story.
+    - start (str): The initial situation or setting of the story.
+    - situations (list): A list of key plot situations or events in the story.
+    - end (str): The resolution or conclusion of the story.
+    - moral (str): The moral lesson or message conveyed by the story.
+
+    "Write a story involving {num_characters} characters. Here's a brief synopsis: {synopsis}. The story is set in {environments}. It begins with {start}, followed by {situations}. Finally, it concludes with {end}. The story's moral is: '{moral}'."
+    Returns:
+    - prompt (str): A story keeping provided parameters in account.
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+
+    prompt = PromptTemplate(template=prompt, input_variables=["num_character","synopsis","environments","start","situations","end","moral"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain    
+
+def generate_story(num_character,synopsis,environments,start,situations,end,moral):
+
+    chat_chain = prompt_for_story()
+
+    response = chat_chain.invoke({num_character,synopsis,environments,start,situations,end,moral}, return_only_outputs=True)
+    return response["output_text"]
+
+
+
+@app.route('/story_prompt', methods=['POST'])
+def story_prompt():
+
+    num_character = request.json['noc']
+    synopsis = request.json['syn']
+    environments = request.json['env']
+    start = request.json['start']
+    situations= request.json['situation']
+    end = request.json['end']
+    moral = request.json['moral']
+
+    response = generate_story(num_character,synopsis,environments,start,situations,end,moral)
+    return jsonify({"response": response})
+
+
+def save(username, password, additional_data):
+
+    user = {'username':username,'password':password,'data':additional_data}
+    with open(r'users.txt', 'a') as file_write:
+        file_write.write(user)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json['username']
+    password = request.json['password']
+    additional_data = request.json['additional_data']
+    
+    if not username or not password:
+        return jsonify({'message': 'Username and password are required.'}), 400
+
+
+    save(username, password, additional_data)
+
+    return jsonify({'message': additional_data})
 
 
 
